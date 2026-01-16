ebf690f374bc4fc2ccb30dfb3289322f051bcf11
AmBoDev <amanbohra885@gmail.com>
2026-01-16 20:56:44 +0530

    first.js

--- first.js
+++ first.js
@@ -1,1 +1,41 @@
 console.log("aman singh bohra ")
+//javascript is case-sensitve and it is unicode (it assign a number to the every charcater)
+//the syntacx of comment in the javascript is same as the c++
+// it is use to wrte a one line comment 
+/*it is used
+*to multi line comment 
+*/
+//DECLERATION
+//VAR,LET,CONST
+// var x=1;
+// if(x==1){
+//     var x=2;
+//     console.log(x);
+// }
+// console.log(x);
+// const y=1;
+// if(y==1){
+//     const y=2;
+//     console.log(y);
+// }
+// console.log(y)
+// let x=1;
+// if(x==1){
+//     let x=3;
+//     console.log(x);
+// }
+// console.log(x);
+// const y=2;
+// y=3; we cant change the variable after declaring it with the const 
+// console.log(y)
+// let y=3;
+// y=4;but we can change the variable after declaring wth the let 
+//let cant be redeclared like let y=5 it will give the error 
+// console.log(y);
+var a=4;
+var a=5;// it can be redeclared but we can not redeclared the const and the let.
+if(true){
+    var a=6;
+    console.log(a);
+}
+console.log(a);
